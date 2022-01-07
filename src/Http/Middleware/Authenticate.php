07c90582780fafa5bdb353c94fd669c8a0e0ae31
juanlee2008 <380410147@qq.com>
2022-01-07 14:40:04 +0800

    first commit

---
+++ src/Http/Middleware/Authenticate.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Http\Middleware;
+
+use Closure;
+
+/** @deprecated */
+class Authenticate extends BaseMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //验证权限是否通过
+//        $this->authenticate($request); //从request表单里获取token，键名为token
+        $this->authenticateFormHeader($request); //从请求头里获取token，键名token
+
+        return $next($request);
+    }
+}
---
+++ src/Http/Middleware/BaseMiddleware.php
@@ -0,0 +1,110 @@
+<?php
+
+/*
+ * This file is part of jwt-auth.
+ *
+ * (c) Sean Tymon <tymon148@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tymon\JWTAuth\Http\Middleware;
+
+use http\Client\Response;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
+
+/** @deprecated */
+abstract class BaseMiddleware
+{
+    /**
+     * The JWT Authenticator.
+     *
+     * @var \Tymon\JWTAuth\JWTAuth
+     */
+    protected $auth;
+
+    /**
+     * Create a new BaseMiddleware instance.
+     *
+     * @param  \Tymon\JWTAuth\JWTAuth  $auth
+     *
+     * @return void
+     */
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Check the request for the presence of a token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\BadRequestHttpException
+     *
+     * @return void
+     */
+    public function checkForToken(Request $request)
+    {
+
+
+        if (! $this->auth->parser()->setRequest($request)->hasToken()) {
+            throw new UnauthorizedHttpException('jwt-auth', '请求字段里没有Token');
+        }
+    }
+
+    /**
+     * Attempt to authenticate a user via the token in the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     *
+     * @throws \Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException
+     *
+     * @return void
+     */
+    public function authenticate(Request $request)
+    {
+        $this->checkForToken($request);
+        try {
+            if (! $this->auth->parseToken()->authenticate()) {
+                throw new UnauthorizedHttpException('jwt-auth', 'User not found');
+            }
+        } catch (JWTException $e) {
+            throw new UnauthorizedHttpException('jwt-auth', $e->getMessage(), $e, $e->getCode());
+        }
+    }
+    public function authenticateFormHeader(Request $request)
+    {
+        if(!$request->hasHeader('token')){
+            throw new UnauthorizedHttpException('jwt-auth', '请携带令牌访问');
+        }
+        try {
+            if (! $this->auth->setToken($request->header('token'))->authenticate()) {
+                throw new UnauthorizedHttpException('jwt-auth', 'User not found');
+            }
+        } catch (JWTException $e) {
+            throw new UnauthorizedHttpException('jwt-auth', $e->getMessage(), $e, $e->getCode());
+        }
+    }
+
+    /**
+     * Set the authentication header.
+     *
+     * @param  \Illuminate\Http\Response|\Illuminate\Http\JsonResponse  $response
+     * @param  string|null  $token
+     *
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     */
+    protected function setAuthenticationHeader($response, $token = null)
+    {
+
+        $token = $token ?: $this->auth->refresh();
+        $response->headers->set('Authorization', 'Bearer '.$token);
+
+        return $response;
+    }
+}
